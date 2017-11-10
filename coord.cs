4e293363299efc94aa022a0b5ab9301d404ed23f
Arcadia2018 <aludovico@arcadia.edu>
2017-11-10 10:16:35 -0500

    get the location of each piece on board

---
+++ coord.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Gates
+{
+  public struct coord
+  {
+    public int x;
+    public int y;
+
+    public coord (int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+}
